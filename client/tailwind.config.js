1816d621536f1163dbee1ca2f56d7445c12a5192
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-04-04 00:41:09 +0530

    client template done

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  ccontent: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
